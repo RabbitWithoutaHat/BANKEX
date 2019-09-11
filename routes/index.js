4ef5d5785c9455cb43c554d6fb58d918c5ca9326
Fekla <rabbdroid@gmail.com>
2019-09-11 17:20:47 +0300

    theme2server

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+// TODO: изменить данный маршрутизатор с использованием AJAX
+router.post('/rolls', (req, res) => {
+  console.log(req.body);
+  console.log(req.body);
+
+  const die = new Die(Number(req.body.rollSide));
+  res.json({
+    die,
+    roll: die.roll(),
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
+
+const PORT = 3000;
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use('/', indexRouter);
+app.listen(PORT, () => console.log(`Started on port: ${PORT}`));
